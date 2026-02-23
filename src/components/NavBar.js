ef5be9026515a31c33dcf25fc13595912f17b3b5
AMAIISDOM\aboudia <amera.boudia@amadeus.com>
2026-02-23 14:41:19 +0100

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,37 +7,45 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container">
+<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className="container">
 
-                <a className="navbar-brand" href='/'><img src={AB} alt="logo..." className="logo"></img></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href='/'>Accueil</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href='/aboutme'>A propos de moi</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" href='/competences'>Mes compétences</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" href='/realisations'>Mes réalisations web</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" href='/contactme'>Contactez moi</a>
-                        </li>
+    <Link className="navbar-brand" to="/">
+      <img src={AB} alt="logo..." className="logo" />
+    </Link>
 
-                    </ul>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
+    </button>
 
-                </div>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-            </div>
-        </nav>
+        <li className="nav-item">
+          <Link className="nav-link active" to="/">Accueil</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link active" to="/aboutme">A propos de moi</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link active" to="/competences">Mes compétences</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link active" to="/realisations">Mes réalisations web</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link active" to="/contactme">Contactez moi</Link>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
     )
 }
 
